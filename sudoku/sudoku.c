997b8ba05482f90b14f8ef0fc84a5f514bf546a9
gyl <15124577336@163.com>
2018-11-30 00:34:40 -0700

    version 2

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-void printBoard(int* board){
-	printf("\n--------- board_temp -------\n");
-	int i;
-	for(i = 0; i < 81; i++){
-		if( i%9 == 0)
-			printf("\n");
-		printf(" %d" , board[i]);
-	}
-}
 
 int64_t now()
 {
@@ -19,43 +10,43 @@ int64_t now()
   return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
+int buddy[81][20] = {-1};
+int board_temp[81] = {0};
+
+
 void solveSudoku(char** board, int boardRowSize, int boardColSize) {
-	int space_row[81] = {-1};
-	int space_col[81] = {-1};
-	int buddy_row[81][20] = {-1};
-	int buddy_col[81][20] = {-1};
+    int space[81] = {-1};
+	//int buddy[81][20] = {-1};
 	int nspaces = 0;
 	int stack[81] = {0};
+	//int board_temp[81] = {0};
 	int i,j;
 	int row,col;
 	for(row=0;row<boardRowSize;row++){
 		for(col = 0;col<boardColSize;col++){
 			int temp = row * 9 + col;
 			if(board[row][col] == '.'){
-				space_row[nspaces] = row;
-				space_col[nspaces] = col;
+				space[nspaces] = temp;
+				board_temp[temp] = 0;
 				int row_start = (row / 3 * 3); // end + 3
 				int col_start = (col / 3 * 3);  // end + 3
 				int counter = 0;
 				for(i = 0;i<boardColSize;i++){
-					if(i != col){				
-						buddy_row[nspaces][counter] = row;
-						buddy_col[nspaces][counter] = i;
+					if(i != col){			
+						buddy[temp][counter] = row * 9 + i;					
 						counter++;
 					}
 				}
 				for(j = 0;j<boardRowSize;j++){
 					if(j != row){
-						buddy_row[nspaces][counter] = j;
-						buddy_col[nspaces][counter] = col;
+						buddy[temp][counter] = j * 9 + col;
 						counter++;					
 					}
 				}
 				for(i = row_start; i < row_start + 3; i++){
 					for(j = col_start; j < col_start + 3; j++){
 						if(i!= row && j!= col){
-							buddy_row[nspaces][counter] = i;
-							buddy_col[nspaces][counter] = j;
+							buddy[temp][counter] = i * 9 + j;
 							counter++;
 						}
 					}		
@@ -63,24 +54,27 @@ void solveSudoku(char** board, int boardRowSize, int boardColSize) {
 				if(counter != 20)
 					printf("counter != 20\n");				
 				nspaces++;
+			}else{
+				board_temp[temp] = board[row][col] - '0';			
 			}				
 		}
 	}
 
-
 	int k = 0;
 	int guess = 0;
 	while(k < nspaces){
-		int temp_row = space_row[k];//index / 9;
-		int temp_col = space_col[k];//index % 9;
+		int index = space[k];
+
+		int temp_row = index / 9;
+		int temp_col = index % 9;
 		int flag = 0;
+
 		for(guess=stack[k] + 1;guess<=9;guess++){
 			// check available
 			int in = 1;
 			for(i = 0;i < 20;i++){
-				int temp_buddy_row = buddy_row[k][i];
-				int temp_buddy_col = buddy_col[k][i];
-				if( guess == board[temp_buddy_row][temp_buddy_col] - '0'){
+				int temp_index = buddy[index][i];
+				if( guess == board_temp[temp_index]){
 					in = 0;	 // not available				
 					break;
 				}
@@ -89,6 +83,7 @@ void solveSudoku(char** board, int boardRowSize, int boardColSize) {
 			if(in == 1){
 				flag = 1;
 				stack[k] = guess; // record check number 1 - 9
+				board_temp[index] = guess;
 				board[temp_row][temp_col] = guess + '0';
 				break;
 			}
@@ -98,11 +93,11 @@ void solveSudoku(char** board, int boardRowSize, int boardColSize) {
 			continue;
 		}
 		else{ // 1 - 9 not available , then back step
+			board_temp[index] = 0;
 			board[temp_row][temp_col] = '.';
 			stack[k] = 0;
 			k--;
 			if(k < 0){
-				printf("No solution\n");
 				break;
 			}		
 		}
@@ -156,8 +151,6 @@ void main(){
 	double sec = (end-start)/1000000.0;
 	printf("%f sec %f ms \n", sec, 1000*sec);
 	testPrint(problem,9,9);
-	//printArr(problem,9,9);
-	//testPrint(problem,9,9);
 }
 
 
