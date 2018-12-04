184cb555783155d85d14640bfe00ef1664b90482
gyl <15124577336@163.com>
2018-12-04 01:09:30 -0700

    static cache

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+
 void printBoard(int* board){
 	printf("\n--------- board_temp -------\n");
 	int i;
@@ -23,7 +24,11 @@ int64_t now()
 int buddy[81][20] = {-1};
 int board_temp[81] = {0};
 
+int space_contain[81][10] = {0}; //
+int quantum_arry[81] = {0}; // 
+
 int quantum(int cell){
+
 	int cache[10] = {0};
 	int i , index;
 	for(i=0;i<20;i++){
@@ -41,30 +46,35 @@ int quantum(int cell){
 
 int findLess(int space[], int nspaces, int depth){
 	int cell = space[depth];
-	int min_value = quantum(cell);
+	//int min_value = quantum(cell);
+	int min_value = quantum_arry[cell];
 	int min_index = depth;
 	int i;
 	for(i=depth+1;i<nspaces&& min_value>1;i++){
-		int temp_value = quantum(space[i]);
+		//int temp_value = quantum(space[i]);
+		int temp_value = quantum_arry[space[i]];
 		if(temp_value < min_value){
 			min_value = temp_value;
 			min_index = i;
 		}
 	}
-	if(min_index != depth){
+	if(min_index != depth){ // equal -- push stack
 		i = space[depth];
 		space[depth] = space[min_index];
 		space[min_index] = i;
 	}
+
 	return space[depth];
 }
 
+int space2space[81][21] = {0};
+
+int stack_positionValue_change[81][21] = {0};
+
 void solveSudoku(char** board, int boardRowSize, int boardColSize) {
     int space[81] = {-1};
-	//int buddy[81][20] = {-1};
 	int nspaces = 0;
 	int stack[81] = {0};
-	//int board_temp[81] = {0};
 	int i,j;
 	int row,col;
 	for(row=0;row<boardRowSize;row++){
@@ -78,7 +88,7 @@ void solveSudoku(char** board, int boardRowSize, int boardColSize) {
 				int counter = 0;
 				for(i = 0;i<boardColSize;i++){
 					if(i != col){			
-						buddy[temp][counter] = row * 9 + i;					
+						buddy[temp][counter] = row * 9 + i;		
 						counter++;
 					}
 				}
@@ -100,10 +110,35 @@ void solveSudoku(char** board, int boardRowSize, int boardColSize) {
 					printf("counter != 20\n");				
 				nspaces++;
 			}else{
-				board_temp[temp] = board[row][col] - '0';			
+				int cell_value = board[row][col] - '0';
+				board_temp[temp] = cell_value;						
 			}				
 		}
 	}
+// pre processing
+	for(i=0;i<81;i++){
+		quantum_arry[i] = 9;
+	}
+	int space_counter = 0;
+	for(i=0;i<nspaces;i++){
+		int space_cell = space[i];
+		space_counter = 0;
+		for(j=0;j<20;j++){
+			int cell_value = board_temp[buddy[space_cell][j]];
+			if(cell_value == 0){
+				space2space[space_cell][space_counter+1] = buddy[space_cell][j];
+				space_counter++;
+			}else{
+				if(space_contain[space_cell][cell_value] == 0){
+					space_contain[space_cell][cell_value] = 1;
+					quantum_arry[space_cell]--;
+				}
+			}
+		}
+		if(space_counter > 0)
+			space2space[space_cell][0] = space_counter;
+	}	
+
 
 	int k = 0;
 	int guess = 0;
@@ -131,6 +166,24 @@ void solveSudoku(char** board, int boardRowSize, int boardColSize) {
 				stack[k] = guess; // record check number 1 - 9
 				board_temp[index] = guess;
 				board[temp_row][temp_col] = guess + '0';
+// ---------------------
+							
+				if(space2space[index][0] > 0){
+					int ii = 0 , j_counter = 0;
+					for(ii = 0;ii<space2space[index][0];ii++){
+						int relate_space_cell = space2space[index][ii+1];
+						if(space_contain[relate_space_cell][guess] == 0){
+							space_contain[relate_space_cell][guess] = 1;
+							quantum_arry[relate_space_cell]--;
+							stack_positionValue_change[k][j_counter+1] = relate_space_cell;
+							j_counter++;
+						}
+					}
+					if(j_counter > 0)
+						stack_positionValue_change[k][0] = j_counter;
+				}
+				
+// -----------------------
 				break;
 			}
 		}
@@ -146,25 +199,23 @@ void solveSudoku(char** board, int boardRowSize, int boardColSize) {
 			board[temp_row][temp_col] = '.';
 			stack[k] = 0;
 			k--;
+// -----------------------
+			
+			if(stack_positionValue_change[k][0] > 0){
+				int ii = 0;
+				for(ii=1;ii<=stack_positionValue_change[k][0];ii++){
+					int change_cell = stack_positionValue_change[k][ii];
+					space_contain[change_cell][stack[k]] = 0;
+					quantum_arry[change_cell]++;
+				}
+			}
+			
+// -----------------------
 			if(k < 0){
 				printf("No solution\n");
 				break;
 			}		
 		}
-	}
-}
-
-void printArr(char** board, int boardRowSize, int boardColSize){
-	int row = 0;
-	int col = 0;	
-	for(row = 0; row < boardRowSize; row++){
-		char* row_index = board[row];
-		for(col = 0; col < boardColSize; col++){
-			if(row_index[col] == '5')
-				row_index[col] = '0';
-			printf("%c , ", row_index[col]);
-		}
-		printf("\n");
 	}
 }
 
@@ -194,6 +245,7 @@ int main(){
 	char row_8[9] = {'.','.','.','4','1','9','.','.','5'};
 	char row_9[9] = {'.','.','.','.','8','.','.','7','9'};
 	*/
+	/*
 	char row_1[9] = {'.','.','9','7','4','8','.','.','.'};
 	char row_2[9] = {'7','.','.','.','.','.','.','.','.'};
 	char row_3[9] = {'.','2','.','1','.','9','.','.','.'};
@@ -203,9 +255,29 @@ int main(){
 	char row_7[9] = {'.','.','.','8','.','3','.','2','.'};
 	char row_8[9] = {'.','.','.','.','.','.','.','.','6'};
 	char row_9[9] = {'.','.','.','2','7','5','9','.','.'};
+	*/
+	/*
+	char row_1[9] = {'.','.','.','.','.','.','.','1','.'};
+	char row_2[9] = {'4','.','.','.','.','.','.','.','.'};
+	char row_3[9] = {'.','2','.','.','.','.','.','.','.'};
+	char row_4[9] = {'.','.','.','.','5','.','4','.','7'};
+	char row_5[9] = {'.','.','8','.','.','.','3','.','.'};
+	char row_6[9] = {'.','.','1','.','9','.','.','.','.'};
+	char row_7[9] = {'3','.','.','4','.','.','2','.','.'};
+	char row_8[9] = {'.','5','.','1','.','.','.','.','.'};
+	char row_9[9] = {'.','.','.','8','.','6','.','.','.'};
+	*/
 	
-
-
+	char row_1[9] = {'.','.','.','2','.','.','.','6','3'};
+	char row_2[9] = {'3','.','.','.','.','5','4','.','1'};
+	char row_3[9] = {'.','.','1','.','.','3','9','8','.'};
+	char row_4[9] = {'.','.','.','.','.','.','.','9','.'};
+	char row_5[9] = {'.','.','.','5','3','8','.','.','.'};
+	char row_6[9] = {'.','3','.','.','.','.','.','.','.'};
+	char row_7[9] = {'.','2','6','3','.','.','5','.','.'};
+	char row_8[9] = {'5','.','3','7','.','.','.','.','8'};
+	char row_9[9] = {'4','7','.','.','.','1','.','.','.'};
+	
 
 	problem[0] = row_1; problem[1] = row_2; problem[2] = row_3; 
 	problem[3] = row_4; problem[4] = row_5; problem[5] = row_6;
@@ -217,6 +289,7 @@ int main(){
 	double sec = (end-start)/1000000.0;
 	printf("%f sec %f ms \n", sec, 1000*sec);
 	testPrint(problem,9,9);
+
 	return 0;
 }
 
